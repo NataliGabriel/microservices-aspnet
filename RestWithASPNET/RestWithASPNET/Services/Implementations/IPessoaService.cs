634551974af7e06d27643491cc88aa4b66b15040
Gabriel Natali <gabriel.natali@inovaipda.org.br>
2022-04-13 09:57:19 -0300

    tutorial udemy

---
+++ RestWithASPNET/RestWithASPNET/Services/Implementations/IPessoaService.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public interface IPessoaService
+    {
+        Pessoa Create(Pessoa pessoa);
+        Pessoa FindByID(long id);
+        List<Pessoa> FindAll();
+        Pessoa Delete(long id);
+        Pessoa Update (Pessoa pessoa);
+
+
+    }
+}
---
+++ RestWithASPNET/RestWithASPNET/Services/Implementations/PessoaServiceImplementation.cs
@@ -0,0 +1,76 @@
+﻿using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Services.Implementations
+{
+    public class PessoaServiceImplementation : IPessoaService
+    {
+        private volatile int count;
+
+        public Pessoa Create(Pessoa pessoa)
+        {
+            return pessoa;
+        }
+
+        public Pessoa Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pessoa> FindAll()
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+            
+            for (int i = 0; i < 8; i++)
+            {
+                Pessoa pessoa = MockPerson(i);
+                pessoas.Add(pessoa);
+            }
+            return pessoas;
+        }
+
+    
+
+        public Pessoa FindByID(long id)
+        {
+            return new Pessoa
+            {
+
+                Id = 1,
+                FirstName = "Gabriel",
+                LastName = "Natali",
+                Address = "Rua Carlos Mazer",
+                Gender = "Male"
+
+
+            };
+        }
+
+        public Pessoa Update(Pessoa pessoa)
+        {
+            return pessoa;
+        }
+
+
+
+
+        private Pessoa MockPerson(int i)
+        {
+            return new Pessoa
+            {
+
+                Id = IncrementAndGet(),
+                FirstName = "Nome Inicial" + i,
+                LastName = "Nome Final" + i,
+                Address = "Algum Endereço" + i,
+                Gender = "Male" + i
+
+
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
